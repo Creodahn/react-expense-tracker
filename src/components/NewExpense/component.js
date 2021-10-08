3bb36bfdf4342329bc813347afb060dc693f192c
creodahn <justin@justindrew.net>
2021-10-08 10:40:44 -0600

    make add new expense togglable

--- src/components/NewExpense/component.js
+++ src/components/NewExpense/component.js
@@ -1,14 +1,25 @@
 import './styles.css';
+import { useState } from 'react';
 import ExpenseForm from './../ExpenseForm/component';
 
 function NewExpense(props) {
+  const [addNewExpense, setAddNewExpense] = useState(false);
+  const onCancelHandler = () => {
+    setAddNewExpense(false);
+  };
+  const onClickHandler = () => {
+    setAddNewExpense(true);
+  };
   const onSaveHandler = (newExpenseData) => {
     props.onSave(newExpenseData);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSave={onSaveHandler}/>
+      {
+        addNewExpense ? <ExpenseForm onCancel={onCancelHandler} onSave={onSaveHandler} />
+        : <button type="button" onClick={onClickHandler}>Add New Expense</button>
+      }
     </div>
   );
 }
